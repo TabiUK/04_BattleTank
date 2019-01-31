c3d89cb76f0bf7b041279e2a4f33dee7744aefe6
Nicholas Panayis <nick@movency.com>
2019-01-31 14:25:01 +0000

    commit

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -3,6 +3,8 @@
 #include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/GameplayStaticsTypes.h"
 
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
@@ -39,9 +41,31 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LauchSpeed)
 	if (Barrel == nullptr) return;
 
 	FVector OutLauchVelocity = FVector(.0);
+	FCollisionResponseParams ResponseParam = FCollisionResponseParams();
+	TArray < AActor * > ActorsToIgnore = TArray < AActor * >();
+
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
 
-	UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LauchSpeed);
+
+	// Calculate the OutLaunchVelocity
+	bool res = UGameplayStatics::SuggestProjectileVelocity
+	(
+		this,
+		OutLauchVelocity,
+		StartLocation,
+		HitLocation,
+		LauchSpeed,
+		false,
+		0.0f,
+		0.0f,
+		ESuggestProjVelocityTraceOption::DoNotTrace
+	);
+
+	if (res) {
+		auto AimDirection = OutLauchVelocity.GetSafeNormal(); // unit vector
+		auto TankName = GetOwner()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Tank %s Aming at %s"), *TankName, *AimDirection.ToString());
+	}
 }
 
 
